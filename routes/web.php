60eb925e7b44e21f617cb8e5626366f6ded113cc
tiarahasnabila <tiarahasnabila@gmail.com>
2025-05-03 11:53:16 +0700

    middleware, gate admin

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,23 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::resource('todo', TodoController::class)->except(['show']);
     Route::get('/todo', [TodoController::class, 'index']) ->name('todo.index');
     Route::get('todo/create', [TodoController::class, 'create']) ->name('todo.create');
     Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
     Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
-    Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
+    // Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
     Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
 
     Route::patch('/todo/{todo}/complete',[TodoController::class, 'complete'])->name('todo.complete');
     Route::patch('/todo/{todo}/incomplete',[TodoController::class, 'uncomplete'])->name('todo.uncomplete');
     Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+});
 
-    Route::get('/user',[UserController::class, 'index'])-> name('user.index');
-    Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::middleware('auth', 'admin')->group(function () {
+    Route::resource('user', UserController::class)->except(['show']);
+  //  Route::get('/user',[UserController::class, 'index'])-> name('user.index');
+  //  Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
     Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
 
